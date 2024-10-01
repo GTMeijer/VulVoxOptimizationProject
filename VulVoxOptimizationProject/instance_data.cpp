f3e940b062fa55c29874c75d2d5815f54994b682
Kuchinawa <gert_meijer89@hotmail.com>
2024-10-01 13:43:11 +0200

    Instance data

---
+++ VulVoxOptimizationProject/instance_data.cpp
@@ -0,0 +1,2 @@
+#include "pch.h"
+#include "instance_data.h"
---
+++ VulVoxOptimizationProject/instance_data.h
@@ -0,0 +1,9 @@
+#pragma once
+
+struct instance_data
+{
+    glm::vec3 position;
+    glm::vec3 rotation;
+    float scale;
+    uint32_t texture_index;
+};
