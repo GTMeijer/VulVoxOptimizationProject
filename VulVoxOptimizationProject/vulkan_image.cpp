a315de8899ada04740fdff6e38884465cc92a904
Kuchinawa <gert_meijer89@hotmail.com>
2024-11-28 09:51:42 +0100

    image sampler clamp

--- VulVoxOptimizationProject/vulkan_image.cpp
+++ VulVoxOptimizationProject/vulkan_image.cpp
@@ -148,9 +148,9 @@ namespace vulvox
         sampler_info.minFilter = VK_FILTER_LINEAR;
 
         //How to handle when the surface is larger than the texture
-        sampler_info.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-        sampler_info.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-        sampler_info.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+        sampler_info.addressModeU = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+        sampler_info.addressModeV = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+        sampler_info.addressModeW = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
 
         VkPhysicalDeviceProperties properties = vulkan_instance->get_physical_device_properties();
 
@@ -170,7 +170,7 @@ namespace vulvox
         sampler_info.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
         sampler_info.mipLodBias = 0.0f;
         sampler_info.minLod = 0.0f;
-        sampler_info.maxLod = 0.0f;
+        sampler_info.maxLod = VK_LOD_CLAMP_NONE;
 
         if (vkCreateSampler(vulkan_instance->device, &sampler_info, nullptr, &sampler) != VK_SUCCESS)
         {
