eaf8fef798137b05224a9625ab0e57216152cf44
Kuchinawa <gert_meijer89@hotmail.com>
2025-01-30 11:11:14 +0100

    changed name

--- VulVoxOptimizationProject/vulkan_instance.cpp
+++ VulVoxOptimizationProject/vulkan_instance.cpp
@@ -19,7 +19,7 @@ namespace vulvox
         app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
         app_info.pApplicationName = "Vulkan"; //Application name, doesnt do much (title is in glfw)
         app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-        app_info.pEngineName = "No Engine";
+        app_info.pEngineName = "VulVox";
         app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
         app_info.apiVersion = VK_API_VERSION_1_0;
 
